594314bb742b456cf6e7e60dd5ac0a91bb581565
tsif <tsiflitzis@gmail.com>
2014-01-16 11:41:35 +0200

    Created interface

--- DTPlaceHolderTextView/DTPlaceHolderTextView.h
+++ DTPlaceHolderTextView/DTPlaceHolderTextView.h
@@ -1,4 +1,17 @@
 /*
+ *
  * DTPlaceHolderTextView.h
  *
  */
+
+
+#import <Foundation/Foundation.h>
+
+@interface DTPlaceHolderTextView : UITextView {
+}
+
+@property (nonatomic, strong) UILabel  *placeHolderLabel;
+@property (nonatomic, strong) NSString *placeHolder;
+@property (nonatomic, strong) UIColor  *placeHolderColor;
+
+@end
